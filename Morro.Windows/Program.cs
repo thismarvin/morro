a171ee804c8cd982770fa940178ab88402f595b7
thismarvin <bend_@outlook.com>
2019-12-18 12:17:06 -0600

    added a new solution for Windows platforms

---
+++ Morro.Windows/Program.cs
@@ -0,0 +1,23 @@
+﻿using Example;
+using System;
+
+namespace Morro.Windows
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Root())
+                game.Run();
+        }
+    }
+#endif
+}

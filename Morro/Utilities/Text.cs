9c89b18e8a51eaad2fde9cd2fcd8e5d45f5dc58b
thismarvin <bend_@outlook.com>
2019-12-19 15:24:51 -0600

    added previous BitmapFont features, but better! 😎

--- Morro/Utilities/Text.cs
+++ Morro/Utilities/Text.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Morro.Core;
+using Morro.Debug;
 using Morro.Graphics;
 using System;
 using System.Collections.Generic;
@@ -8,36 +9,82 @@ using System.Text;
 
 namespace Morro.Utilities
 {
-    class Text : MonoObject, IDisposable
+    class Text : MonoObject, IDisposable, IDebugable
     {
         public string Content { get; private set; }
+        public Vector2 Scale { get; private set; }
+        public float Rotation { get; private set; }
 
         private readonly BMFont font;
         private readonly BMFontShader shader;
         private readonly List<Sprite> sprites;
+        private Matrix transform;
+
+        private readonly AABB literalBounds;
+        private readonly Quad exactBounds;
+        private readonly AABB broadBounds;
 
         public Text(float x, float y, string content, string fontName) : base(x, y, 1, 1)
         {
             Content = content;
             font = AssetManager.GetFont(fontName);
+            Scale = new Vector2(1, 1);
+            transform = Matrix.Identity;
 
             sprites = new List<Sprite>();
             shader = new BMFontShader(Color.White, Color.Black, Color.Transparent);
+
+            literalBounds = new AABB(X, Y, Width, Height, 1, PICO8.GrassGreen, VertexInformation.Dynamic);
+            exactBounds = new Quad(X, Y, Width, Height, 1, PICO8.BloodRed, VertexInformation.Dynamic);
+            broadBounds = new AABB(X, Y, Width, Height, 1, PICO8.FleshPink, VertexInformation.Dynamic);
 
             CreateText();
         }
 
         public override void SetLocation(float x, float y)
         {
+            if (X == x && Y == y)
+                return;
+
             base.SetLocation(x, y);
-            CreateText();
+
+            UpdateText();
         }
 
         public void SetContent(string content)
         {
+            if (Content == content)
+                return;
+
             Content = content;
 
             CreateText();
+        }
+
+        public void SetScale(float xScale, float yScale)
+        {
+            if (Scale.X == xScale && Scale.Y == yScale)
+                return;
+
+            Scale = new Vector2(xScale, yScale);
+
+            UpdateText();
+        }
+
+        public void SetRotation(float rotation)
+        {
+            if (Rotation == rotation)
+                return;
+
+            Rotation = rotation;
+
+            transform =
+                Matrix.CreateTranslation(-Center.X, -Center.Y, 0) *
+                Matrix.CreateRotationZ(Rotation) *
+                Matrix.CreateTranslation(Center.X, Center.Y, 0) *
+                Matrix.Identity;
+
+            UpdateText();
         }
 
         public void SetStyling(Color textColor, Color outlineColor, Color aaColor)
@@ -45,34 +92,139 @@ namespace Morro.Utilities
             shader.SetTextColor(textColor);
             shader.SetOutlineColor(outlineColor);
             shader.SetAAColor(aaColor);
-
-            CreateText();
         }
 
         private void CreateText()
         {
             sprites.Clear();
 
-            float x = X;
+            float xFinal = X;
             char character;
             BMFontCharacter characterData;
             Sprite sprite;
-           
+
             for (int i = 0; i < Content.Length; i++)
-            {                
+            {
                 character = Content.Substring(i, 1).ToCharArray()[0];
                 characterData = font.GetCharacterData(character);
 
-                sprite = new Sprite(x + characterData.XOffset, Y + characterData.YOffset, font.FontFace + " " + (int)character)
+                sprite = new Sprite(xFinal + characterData.XOffset * Scale.X, Y + characterData.YOffset * Scale.Y, font.FontFace + " " + (int)character)
                 {
-                    Effect = shader.Effect
+                    Effect = shader.Effect,
+                    Scale = Scale,
+                    Rotation = Rotation
                 };
+
                 sprites.Add(sprite);
 
-                x += characterData.XAdvance;
+                xFinal += characterData.XAdvance * Scale.X;
             }
 
-            SetDimensions((int)Math.Ceiling(x - X), font.Size);
+            SetDimensions((int)Math.Ceiling(xFinal - X), (int)Math.Ceiling(font.Size * Scale.Y));
+            exactBounds.SetBounds(X, Y, Width, Height);
+            literalBounds.SetBounds(X, Y, Width, Height);
+        }
+
+        private void UpdateText()
+        {
+            float xFinal = X;
+            char character;
+            BMFontCharacter characterData;
+            Vector2 result;
+
+            for (int i = 0; i < Content.Length; i++)
+            {
+                character = Content.Substring(i, 1).ToCharArray()[0];
+                characterData = font.GetCharacterData(character);
+
+                sprites[i].SetLocation(xFinal + characterData.XOffset * Scale.X, Y + characterData.YOffset * Scale.Y);
+                sprites[i].Scale = Scale;
+                sprites[i].Rotation = Rotation;
+
+                if (Rotation != 0)
+                {
+                    result = Vector2.Transform(sprites[i].Position, transform);
+                    sprites[i].SetLocation(result.X, result.Y);
+                }
+
+                xFinal += characterData.XAdvance * Scale.X;
+            }
+
+            SetDimensions((int)Math.Ceiling(xFinal - X), (int)Math.Ceiling(font.Size * Scale.Y));
+            exactBounds.SetBounds(X, Y, Width, Height);
+            literalBounds.SetBounds(X, Y, Width, Height);
+
+            HandleRotation();
+        }
+
+        private void HandleRotation()
+        {
+            if (Rotation == 0)
+                return;
+
+            exactBounds.SetRotationOffset(Width / 2, Height / 2);
+            exactBounds.SetRotation(Rotation);
+            exactBounds.SetupForCollisionTesting();
+
+            float xMin = VertexFinder(exactBounds.TransformedVertices, "x", "minimum");
+            float xMax = VertexFinder(exactBounds.TransformedVertices, "x", "maximum");
+            float yMin = VertexFinder(exactBounds.TransformedVertices, "y", "minimum");
+            float yMax = VertexFinder(exactBounds.TransformedVertices, "y", "maximum");
+            float width = xMax - xMin;
+            float height = yMax - yMin;
+
+            broadBounds.SetBounds(xMin, yMin, (int)Math.Ceiling(width), (int)Math.Ceiling(height));
+        }
+
+        private float VertexFinder(Vector2[] vertices, string dimension, string qualifier)
+        {
+            float result = GetValueOf(0);
+
+            for (int i = 1; i < vertices.Length; i++)
+            {
+                if (Valid(GetValueOf(i)))
+                {
+                    result = GetValueOf(i);
+                }
+            }
+            return result;
+
+            float GetValueOf(int index)
+            {
+                string formatted = dimension.ToLowerInvariant();
+
+                if (formatted == "x")
+                {
+                    return vertices[index].X;
+                }
+                else if (formatted == "y")
+                {
+                    return vertices[index].Y;
+                }
+                throw new ArgumentException();
+            }
+
+            bool Valid(float value)
+            {
+                string formatted = qualifier.ToLowerInvariant();
+
+                if (formatted == "minimum")
+                {
+                    return value < result;
+                }
+                else if (formatted == "maximum")
+                {
+                    return value > result;
+                }
+                throw new ArgumentException();
+            }
+        }
+
+        public void Debug(SpriteBatch spriteBatch, Camera camera)
+        {
+            exactBounds.Draw(spriteBatch, camera);
+            broadBounds.Draw(spriteBatch, camera);
+            literalBounds.Draw(spriteBatch, camera);
         }
 
         public void Draw(SpriteBatch spriteBatch, CameraType cameraType)
